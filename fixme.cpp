70b488520dd1108980f5e523fcb394e78e8d156e
trustworthyq <dolgiy_m@list.ru>
2024-04-21 22:07:14 +0300

    change file

--- fixme.cpp
+++ fixme.cpp
@@ -8,7 +8,7 @@
 #include "../utils/constants/FieldCnst.h"
 #include "../utils/constants/CollectionCnst.h"
 #include "../utils/constants/UsersSettingsCnst.h"
-
+#include "aaa"
 using bsoncxx::builder::basic::kvp;
 
 std::map<std::string, Session> SessionAgregator::currentConnections;
